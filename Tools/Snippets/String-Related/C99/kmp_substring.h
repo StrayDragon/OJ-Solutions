5a9c3beda2a62caef9dc2bb77ed1879f1dec7e5a
Peilong Li <straydragonl@foxmail.com>
2019-03-06 22:27:01 +0800

    planning: kmp_substring (not finished)

---
+++ Tools/Snippets/String-Related/C99/kmp_substring.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string.h>
+
+typedef int Index;
+const int SubStrNotFound = -1;
+
+Index kmp_substring(char* text, int text_len, char* patten, int pattern_len) {
+  
+  return SubStrNotFound;
+}
