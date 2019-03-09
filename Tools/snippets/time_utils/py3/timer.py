666e1e595e99768865e3494afad7c6d142a83f16
straydragon <straydragonl@foxmail.com>
2019-03-09 17:08:56 +0800

    add a util for py3

---
+++ Tools/snippets/time_utils/py3/timer.py
@@ -0,0 +1,36 @@
+# Copy from https://python3-cookbook.readthedocs.io/zh_CN/latest/c13/p13_making_stopwatch_timer.html
+# Copyright © 2013 David Beazley and Brian Jones. All rights reserved.
+import time
+
+
+class Timer:
+    def __init__(self, func=time.perf_counter):
+        self.elapsed = 0.0
+        self._func = func
+        self._start = None
+
+    def start(self):
+        if self._start is not None:
+            raise RuntimeError('Already started')
+        self._start = self._func()
+
+    def stop(self):
+        if self._start is None:
+            raise RuntimeError('Not started')
+        end = self._func()
+        self.elapsed += end - self._start
+        self._start = None
+
+    def reset(self):
+        self.elapsed = 0.0
+
+    @property
+    def running(self):
+        return self._start is not None
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
