6637b7786a7c1875e45be090055667e1a265988b
Peilong Li <straydragonl@foxmail.com>
2019-03-29 23:14:35 +0800

    update

--- tools/assistant/model/__init__.py
+++ tools/assistant/model/__init__.py
@@ -1,12 +1,12 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod, ABC
 
 
-class Serializable(metaclass=ABCMeta):
+class Serializable(ABC):
     @abstractmethod
     def to_json(self) -> dict:
         pass
 
     @classmethod
+    @abstractmethod
     def from_json(cls, json_obj: dict):
-        # TODO:如何编写反序列化
         pass
