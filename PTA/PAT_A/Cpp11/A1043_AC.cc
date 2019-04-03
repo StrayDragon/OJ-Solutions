11d88b1b8bbb17f9b5e7ba1cef17aabfa4b8c45b
straydragon <straydragonl@foxmail.com>
2019-04-03 15:54:19 +0800

    first pass

---
+++ PTA/PAT_A/Cpp11/A1043_AC.cc
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  using Ptr = shared_ptr<TreeNode>;
+  int data;
+  Ptr lchild, rchild;
+
+  TreeNode() = default;
+  TreeNode(int d) : data(d) {}
+};
+
+void insert(TreeNode::Ptr& root, int data) {
+  if (root == nullptr) {
+    root = make_shared<TreeNode>(data);
+    return;
+  }
+  if (data < root->data)
+    insert(root->lchild, data);
+  else
+    insert(root->rchild, data);
+}
+
+void pre_order(const TreeNode::Ptr& root, vector<int>& result) {
+  if (root == nullptr)
+    return;
+  result.push_back(root->data);
+  pre_order(root->lchild, result);
+  pre_order(root->rchild, result);
+}
+
+void pre_order_mirror(const TreeNode::Ptr& root, vector<int>& result) {
+  if (root == nullptr)
+    return;
+  result.push_back(root->data);
+  pre_order_mirror(root->rchild, result);
+  pre_order_mirror(root->lchild, result);
+}
+
+void post_order(const TreeNode::Ptr& root, vector<int>& result) {
+  if (root == nullptr)
+    return;
+  post_order(root->lchild, result);
+  post_order(root->rchild, result);
+  result.push_back(root->data);
+}
+
+void post_order_mirror(const TreeNode::Ptr& root, vector<int>& result) {
+  if (root == nullptr)
+    return;
+  post_order_mirror(root->rchild, result);
+  post_order_mirror(root->lchild, result);
+  result.push_back(root->data);
+}
+
+vector<int> source, pre, pre_m, post, post_m;
+
+int main() {
+  TreeNode::Ptr root;
+  int n;
+  cin >> n;
+  for (int i = 0, data; i < n; i++) {
+    cin >> data;
+    source.push_back(data);
+    insert(root, data);
+  }
+  pre_order(root, pre);
+  pre_order_mirror(root, pre_m);
+  post_order(root, post);
+  post_order_mirror(root, post_m);
+  if (source == pre) {
+    cout << "YES\n";
+    for (int i = 0; i < post.size(); i++) {
+      cout << post[i];
+      if (i < post.size() - 1)
+        cout << " ";
+    }
+  } else if (source == pre_m) {
+    cout << "YES\n";
+    for (int i = 0; i < post_m.size(); i++) {
+      cout << post_m[i];
+      if (i < post_m.size() - 1)
+        cout << " ";
+    }
+  } else {
+    cout << "NO\n";
+  }
+
+  return 0;
+}
---
+++ PTA/PAT_A/Cpp11/A1094_AC.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAXN = 110;
+vector<int> nodes[MAXN];
+int table[MAXN] = {0};
+
+void dfs(int index, int level) {
+  table[level]++;
+  for (int i = 0; i < nodes[index].size(); i++)
+    dfs(nodes[index][i], level + 1);
+}
+
+int main() {
+  int n, m, parent, k, child;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++) {
+    cin >> parent >> k;
+    for (int j = 0; j < k; j++) {
+      cin >> child;
+      nodes[parent].push_back(child);
+    }
+  }
+
+  dfs(1, 1);
+  int max_level = -1, max_value = 0;
+  for (int i = 1; i < MAXN; i++) {
+    if (table[i] > max_value) {
+      max_value = table[i];
+      max_level = i;
+    }
+  }
+
+  cout << max_value << " " << max_level << '\n';
+  return 0;
+}
