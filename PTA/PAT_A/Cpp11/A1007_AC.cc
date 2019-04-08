f8306cf474782e5d717ab4691f1a27fa1e67b075
straydragon <straydragonl@foxmail.com>
2019-04-08 15:38:49 +0800

    first pass

---
+++ PTA/PAT_A/Cpp11/A1007_AC.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+const int MAXK = 10010;
+
+int k, seq[MAXK];
+
+int main() {
+  cin >> k;
+  bool all_negative = true;
+  for (int i = 0; i < k; i++) {
+    cin >> seq[i];
+    if (all_negative && seq[i] >= 0)
+      all_negative = false;
+  }
+
+  int maxsum = -1;
+  int left = k - 1, right = k - 1;
+  for (int i = 0, sum = 0, it = 0; i < k; i++) {
+    sum += seq[i];
+    if (maxsum < sum) {
+      maxsum = sum;
+      left = it;
+      right = i;
+    } else if (sum < 0) {
+      it = i + 1;
+      sum = 0;
+    }
+  }
+
+  if (all_negative) {
+    cout << "0 " << seq[0] << " " << seq[k - 1] << '\n';
+  } else {
+    cout << maxsum << " " << seq[left] << " " << seq[right] << '\n';
+  }
+  return 0;
+}
---
+++ PTA/PAT_A/Cpp11/A1051_AC.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int m, n, k;
+int main() {
+  cin >> m >> n >> k;
+  for (int i = 0; i < k; i++) {
+    vector<int> nums;
+    for (int _ = 0, e; _ < n; _++) {
+      cin >> e;
+      nums.push_back(e);
+    }
+
+    stack<int> stk;
+    bool invalid = false;
+    for (int cur = 0, j = 1; j <= n; j++) {
+      stk.push(j);
+      if (stk.size() > m) {
+        invalid = true;
+        break;
+      }
+
+      while (!stk.empty() && stk.top() == nums[cur]) {
+        stk.pop();
+        cur++;
+      }
+    }
+
+    if (invalid || stk.size() != 0)
+      cout << "NO\n";
+    else
+      cout << "YES\n";
+  }
+
+  return 0;
+}
