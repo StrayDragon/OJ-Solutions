2223c4281baf0198b1e049797a2572e0fef0d88a
straydragon <straydragonl@foxmail.com>
2019-04-19 21:02:03 +0800

    update

--- unstable/Other/D.cc
+++ unstable/Other/D.cc
@@ -1,94 +1,33 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
-#define SIZE 256
-
-int MoveByGS(int j, int m, int suffix[], bool prefix[]) {
-  int k = m - j - 1;  // 好后缀长度
-  if (suffix[k] != -1) return j + 1 - suffix[k];
-
-  for (int r = j + 2; r < m; r++) {
-    if (prefix[m - r] == true) return r;
-  }
-
-  return m;
-}
-
-// 生成好后缀数组
-void GenerateGS(char str[], int m, int suffix[], bool prefix[]) {
-  for (int i = 0; i < m; i++) {
-    suffix[i] = -1;
-    prefix[i] = false;
-  }
-
-  // [0, i] 的子串和模式串求公共后缀子串
-  for (int i = 0; i < m - 1; i++) {
-    int j = i;
-    int k = 0;
-    while (j >= 0 && str[j] == str[m - 1 - k])  // 下标都向前移动
-    {
-      j--;
-      k++;
+bool sunday(char* text, char* patt) {
+  size_t temp[256];
+  size_t* shift = temp;
+  size_t i, patt_size = strlen(patt), text_size = strlen(text);
+  for (i = 0; i < 256; i++)
+    *(shift + i) = patt_size + 1;
+  for (i = 0; i < patt_size; i++)
+    *(shift + char(*(patt + i))) = patt_size - i;
+  size_t limit = text_size - patt_size + 1;
+  for (i = 0; i < limit; i += shift[int(text[i + patt_size])])
+    if (text[i] == *patt) {
+      char* match_text = text + i + 1;
+      size_t match_size = 1;
+      do {  // 输出所有匹配的位置
+        if (match_size == patt_size) {
+          return true;
+        }
+      } while ((*match_text++) == patt[match_size++]);
     }
-
-    if (k != 0) suffix[k] = j + 1;  // 公共后缀子串的起始位置
-    if (j == -1) prefix[k] = true;  // 公共后缀子串同时也是模式串的前缀子串
-  }
+  return false;
 }
 
-// 生成坏字符对应的散列表
-void GenerateBC(char str[], int m, int bc[]) {
-  // 所有字符初始化为 -1
-  for (int i = 0; i < SIZE; i++) {
-    bc[i] = -1;
-  }
-
-  for (int i = 0; i < m; i++) {
-    int ascii = str[i] - '\0';  // 求出字符对应的 ASCII 码
-    bc[ascii] = i;
-  }
-}
-
-int BM(char str1[], int n, char str2[], int m) {
-  int bc[SIZE];  // 记录每个字符在模式串中最后出现的位置，作为坏字符散列表
-  GenerateBC(str2, m, bc);
-
-  int suffix[m];
-  bool prefix[m];
-  GenerateGS(str2, m, suffix, prefix);
-
-  int i = 0;    // 表示主串和模式串对齐时第一个字符的位置
-  int si = 0;   // 坏字符对应于模式串中的位置
-  int xi = -1;  // 坏字符在模式串中最后出现的位置
-
-  while (i <= n - m) {
-    int j = 0;
-    // 从后向前进行匹配
-    for (j = m - 1; j >= 0; j--) {
-      // 找到了第一个不匹配的字符
-      if (str1[i + j] != str2[j]) break;
-    }
-
-    if (j < 0) return i;  // 匹配成功
-
-    si = j;
-    xi = bc[str1[i + j] - '\0'];
-    int x = si - xi;  // 坏字符规则应该向后移动的位数
-    int y = 0;        // 好后缀规则应该向后移动的位数
-
-    if (j < m - 1) y = MoveByGS(j, m, suffix, prefix);
-
-    x = x > y ? x : y;
-    i = i + x;
-  }
-
-  return -1;
-}
-
-char passwords[20000][20];
+char passwords[20010][15];
 int main() {
-  for (int i = 0; i < 20000; i++)
-    for (int j = 0; j < 20; j++) passwords[i][j] = '\0';
+  for (int i = 0; i < 20010; i++)
+    for (int j = 0; j < 15; j++)
+      passwords[i][j] = '\0';
 
   int n;
   scanf("%d\n", &n);
@@ -106,12 +45,12 @@ int main() {
   scanf("%d", &m);
   for (int case_cnt = 1; case_cnt <= m; case_cnt++) {
     printf("Case #%d:\n", case_cnt);
-    char guess[11] = {0};
+    char guess[15] = {0};
     scanf("%s", guess);
     getchar();
     int matched = 0;
     for (int i = 0; i < n; i++) {
-      if (BM(passwords[i], strlen(passwords[i]), guess, strlen(guess)) != -1) {
+      if (sunday(passwords[i], guess)) {
         matched = 1;
         break;
       }
