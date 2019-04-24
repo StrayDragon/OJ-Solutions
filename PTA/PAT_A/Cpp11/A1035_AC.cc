578bdfe66da963324f5f35bfd90a42de93d522f2
straydragon <straydragonl@foxmail.com>
2019-04-24 16:33:32 +0800

    first pass

---
+++ PTA/PAT_A/Cpp11/A1035_AC.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+
+vector<pair<char, char>> couples{{'1', '@'},
+                                 {'0', '%'},
+                                 {'l', 'L'},
+                                 {'O', 'o'}};
+vector<tuple<string, string, bool>> accounts;
+
+void replace_confused_char(string& s, bool& modified) {
+  for (auto& couple : couples) {
+    auto pos = s.find(couple.first);
+    while (pos != string::npos) {
+      s[pos] = couple.second;
+      modified = true;
+      pos = s.find(couple.first, pos + 1);
+    }
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string name, passwd;
+  bool some_modified = false;
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    bool is_modified = false;
+    cin >> name >> passwd;
+    replace_confused_char(passwd, is_modified);
+    if (!some_modified && is_modified)
+      some_modified = is_modified;
+    if (is_modified)
+      cnt++;
+    accounts.push_back(tuple<string, string, bool>(name, passwd, is_modified));
+  }
+
+  if (some_modified) {
+    cout << cnt << '\n';
+    for (auto& account : accounts) {
+      if (get<2>(account)) {
+        cout << get<0>(account) << " " << get<1>(account) << '\n';
+      }
+    }
+  } else {
+    int len = accounts.size();
+    if (len == 1) {
+      printf("There is 1 account and no account is modified\n");
+    } else {
+      printf("There are %d accounts and no account is modified\n", len);
+    }
+  }
+
+  return 0;
+}
---
+++ PTA/PAT_A/Cpp11/A1036_AC.cc
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+#define ALL(x) x.begin(), x.end()
+struct Student {
+  string name;
+  char gender;
+  string id;
+  int grade;
+
+  bool operator<(const Student& other) { return grade < other.grade; }
+};
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<Student> students;
+  for (Student t; n--;) {
+    cin >> t.name >> t.gender >> t.id >> t.grade;
+    students.push_back(t);
+  }
+
+  sort(ALL(students));
+
+  string result;
+
+  bool is_absent1 = true;
+  int grade_f;
+  for (int i = students.size(); i >= 0; --i) {
+    if (students[i].gender == 'F') {
+      result += students[i].name + " " + students[i].id + "\n";
+      is_absent1 = false;
+      grade_f = students[i].grade;
+      break;
+    }
+  }
+
+  if (is_absent1) {
+    result += "Absent\n";
+  }
+
+  int grade_m;
+  int is_absent2 = true;
+  for (auto& student : students) {
+    if (student.gender == 'M') {
+      result += student.name + " " + student.id + "\n";
+      is_absent2 = false;
+      grade_m = student.grade;
+      break;
+    }
+  }
+
+  if (is_absent2) {
+    result += "Absent\n";
+  }
+
+  cout << result;
+  if (is_absent1 || is_absent2) {
+    cout << "NA\n";
+  } else {
+    cout << grade_f - grade_m << '\n';
+  }
+
+  return 0;
+}
