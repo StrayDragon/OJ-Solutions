969a3ef2537fc19985ad04f7feedafe51d24cddd
straydragon <straydragonl@foxmail.com>
2019-04-30 20:53:17 +0800

    first pass

---
+++ PTA/PAT_A/Cpp11/A1116_AC.cc
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+bool is_prime(int n) {
+  if (n <= 1)
+    return false;
+  int bound = (int)std::sqrt(1.0 * n);
+  for (int i = 2; i <= bound; ++i)
+    if (n % i == 0)
+      return false;
+  return true;
+}
+
+int main() {
+  unordered_map<int, string> awarddict;
+  unordered_map<int, bool> checkeddict;
+  int n;
+  cin >> n;
+  for (int id, i = 1; i <= n; i++) {
+    cin >> id;
+    if (i == 1) {
+      awarddict[id] = "Mystery Award";
+    } else {
+      if (is_prime(i)) {
+        awarddict[id] = "Minion";
+      } else {
+        awarddict[id] = "Chocolate";
+      }
+    }
+  }
+
+  int k;
+  cin >> k;
+  for (int id; k--;) {
+    cin >> id;
+    printf("%04d: ", id);
+    if (awarddict.find(id) != awarddict.end()) {
+      if (checkeddict.find(id) != checkeddict.end()) {
+        cout << "Checked\n";
+      } else {
+        checkeddict[id] = true;
+        cout << awarddict[id] << '\n';
+      }
+    } else {
+      cout << "Are you kidding?\n";
+    }
+  }
+  return 0;
+}
---
+++ PTA/PAT_A/Cpp11/A1120_AC.cc
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> friend_ids;
+  int n;
+  cin >> n;
+  for (string digits; n--;) {
+    cin >> digits;
+    int tmp_sum = 0;
+    for (auto& digit : digits)
+      tmp_sum += digit - '0';
+    friend_ids.push_back(tmp_sum);
+  }
+  std::sort(friend_ids.begin(), friend_ids.end());
+  auto last = std::unique(friend_ids.begin(), friend_ids.end());
+  cout << std::distance(friend_ids.begin(), last) << '\n';
+  cout << *friend_ids.begin();
+  std::for_each(friend_ids.begin() + 1, last,
+                [](int id) { cout << " " << id; });
+
+  return 0;
+}
