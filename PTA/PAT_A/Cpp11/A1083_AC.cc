51371e4e5ef04201e3ed0bdea401fdfd9d62100b
straydragon <straydragonl@foxmail.com>
2019-06-06 08:57:45 +0800

    first pass: A1083

---
+++ PTA/PAT_A/Cpp11/A1083_AC.cc
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+using Student = tuple<string, string, int>;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<Student> students;
+  int grade, grade1, grade2;
+  for (string name, id; n--;) {
+    cin >> name >> id >> grade;
+    students.push_back(make_tuple(name, id, grade));
+  }
+  cin >> grade1 >> grade2;
+  std::sort(students.begin(), students.end(), [](Student& lhs, Student& rhs) {
+    return std::get<2>(lhs) > std::get<2>(rhs);
+  });
+
+  bool not_matched = true;
+  for (auto& it : students) {
+    if (grade1 <= std::get<2>(it) && std::get<2>(it) <= grade2) {
+      cout << std::get<0>(it) << " " << std::get<1>(it) << '\n';
+      not_matched = false;
+    }
+  }
+
+  if (not_matched)
+    cout << "NONE\n";
+}
