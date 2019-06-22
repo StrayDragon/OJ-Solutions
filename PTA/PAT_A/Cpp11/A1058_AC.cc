5b5c3a4439d4a2bcb8a420ccf905a388dbfb3ef9
straydragon <straydragonl@foxmail.com>
2019-06-22 09:06:18 +0800

    first pass: A1058

---
+++ PTA/PAT_A/Cpp11/A1058_AC.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  int g1, s1, k1, g2, s2, k2;
+  scanf("%d.%d.%d %d.%d.%d", &g1, &s1, &k1, &g2, &s2, &k2);
+  int k = (k1 + k2) % 29, carry = (k1 + k2) / 29;
+  int s = (s1 + s2 + carry) % 17;
+  carry = (s1 + s2 + carry) / 17;
+  int g = g1 + g2 + carry;
+  printf("%d.%d.%d\n", g, s, k);
+  return 0;
+}
