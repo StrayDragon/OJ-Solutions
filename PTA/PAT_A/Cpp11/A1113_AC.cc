c1aa7183d019dabb6b241e162a47312f0ffb3908
straydragon <straydragonl@foxmail.com>
2019-06-29 10:12:45 +0800

    first pass: A1113

---
+++ PTA/PAT_A/Cpp11/A1113_AC.cc
@@ -0,0 +1,29 @@
+// reference: https://www.liuchuo.net/archives/2299
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  size_t n;
+  cin >> n;
+
+  int sum = 0;
+  vector<int> nums(n);
+  for (size_t i = 0; i < n; i++) {
+    cin >> nums[i];
+    sum += nums[i];
+  }
+
+  std::sort(nums.begin(), nums.end());
+
+  int haftsum = 0;
+  for (size_t i = 0; i < n / 2; i++) {
+    haftsum += nums[i];
+  }
+  cout << (n % 2) << " " << sum - 2 * haftsum;
+
+  return 0;
+}
