39817c0ded04900ad140cd9c78fc9005b70c633e
straydragon <straydragonl@foxmail.com>
2019-07-19 21:05:10 +0800

    first pass: A1038

---
+++ PTA/PAT_A/Cpp11/A1038_AC.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> nums(10010);
+  for (int i = 0; i < n; i++) cin >> nums[i];
+
+  std::sort(nums.begin(), nums.begin() + n,
+            [](string& lhs, string& rhs) { return lhs + rhs < rhs + lhs; });
+  string res;
+  for (auto&& num : nums) res += num;
+  for (; res.size() != 0 && res[0] == '0';) res.erase(res.begin());
+
+  cout << (res.size() == 0 ? "0" : res);
+  return 0;
+}
